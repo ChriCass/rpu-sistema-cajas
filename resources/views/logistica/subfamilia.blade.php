d4e3176f151aa8b56e09e92140e254bd47d7eaf3
ChriCass <chris_ccc68@yahoo.com.pe>
2024-08-03 16:56:32 -0500

    UPDATED: mejoras en front

--- resources/views/logistica/subfamilia.blade.php
+++ resources/views/logistica/subfamilia.blade.php
@@ -7,7 +7,7 @@
 
     <div class="max-w-screen-xl mx-auto px-4 mt-12">
         <div class="flex flex-wrap justify-center -mx-4">
-            <div class="w-full sm:w-6/12 px-4 mb-4">
+            <div class="w-full sm:w-8/12 px-4 mb-4">
                 <div class="bg-white rounded-md p-4 shadow-lg">
                     @livewire('sub-familia-table')
                 </div>
