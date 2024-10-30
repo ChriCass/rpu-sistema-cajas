aa7df75f3329beb8e53810606247a4d90e91d5de
ChriCass <chris_ccc68@yahoo.com.pe>
2024-10-30 18:10:52 -0500

    ADDED: RegistroDocAvanzServie empty

---
+++ app/services/RegistroDocAvanzService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+ 
+
+class RegistroDocAvanzService
+{
+    //////AQUI SE IMPLEMENTA CODIGO DE INSERSION DE DATOS EN EL REGISTRO AVANZADO - APLICA PARA INGRESO/EGRESO/CXP/CXC
+     
+}
